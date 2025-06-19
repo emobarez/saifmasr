c93babbe1479ff88d2bf1b0a7b8e42ca56695dbc
Essam Mobarez <mobarezessam555@gmail.com>
2025-06-19 10:52:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.canva.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
